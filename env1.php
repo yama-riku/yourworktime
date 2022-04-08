793da4bdab4cfa010b54eb2b7df3e7637439a10a
riku <yama_moto_g_osaka@icloud.com>
2022-04-08 18:00:20 +0900

    fix viewport

--- env1.php
+++ env1.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST','us-cdbr-east-05.cleardb.net');
-define('DB_NAME','heroku_723a4ca48f6a365');
-define('DB_USER','bbbb26d5e07e9e');
-define('DB_PASS','b88eb2f6');
+define('DB_HOST','localhost');
+define('DB_NAME','worktime');
+define('DB_USER','yamari');
+define('DB_PASS','Mayoineko1');
 
 ?>
--- public/signup_form.php
+++ public/signup_form.php
@@ -27,7 +27,7 @@ unset($_SESSION['login_err']);
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,viewport-fit=cover">
     <link rel = "stylesheet" href = "signup_form.css">
     <title>worktime/新規登録画面</title>
 </head>
